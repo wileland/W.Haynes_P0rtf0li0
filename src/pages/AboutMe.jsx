b827844b697a501bd88eec9dab173996f61909d1
wileland <wileland7@gmail.com>
2024-01-19 18:51:42 -0600

    Update project and page styles for responsiveness and accessibility

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import "./AboutMe.css";
 
 function AboutMe() {
-  // This is a placeholder for your actual content
   return (
     <section className="about-me">
       <img
-        src="/path-to-your-avatar.jpg" /*ADD AVATAR HERE*/
-        alt="Your Avatar"
+        src="/path-to-your-avatar.jpg" // Replace with the path to your actual avatar
+        alt="William L. Haynes"
         className="about-me-avatar"
       />
-      <h1>Hi, I'm [William L. Haynes]</h1>
+      <h1>Hi, I'm William L. Haynes</h1>
       <p>
-        I am a web developer with a passion for creating beautiful and
-        functional web applications. Here you can find some of the work I've
-        done and get in touch with me for future projects or collaborations.
+        With a decade dedicated to shaping minds as an English and ESOL teacher
+        in inner-city San Antonio, I bring a wealth of experience to the table.
+        My academic journey, crowned with a BA and MA in English from Texas A&M
+        International University, underpins my creative and analytical prowess.
+        I thrive on intuition and creativity, which fuels my ability to think
+        outside the box and devise innovative solutions. Beyond the science of
+        education, my teaching career has honed my leadership skills, instilling
+        in me the fine art of guiding and inspiring. From managing time to
+        orchestrating behavior, my path as an educator has been as enlightening
+        as it has been fulfilling.
       </p>
       {/* Add more about your experience, education, skills, etc. */}
     </section>
