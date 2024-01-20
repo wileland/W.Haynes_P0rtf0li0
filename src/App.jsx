45acab670674364b494ec30c263511707285fbdb
wileland <wileland7@gmail.com>
2024-01-19 19:00:14 -0600

    Refactor code structure and remove unused state

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Navigation from "./components/Navigation/Navigation";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutMe from "./pages/AboutMe";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  // State for the counter, if you want to keep this feature
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <div className="App">
         <Header />
-        <Navigation />
         <main>
-          {/* The Routes component renders the matching route */}
           <Routes>
             <Route path="/" element={<AboutMe />} />
             <Route path="/portfolio" element={<Portfolio />} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; 
-import "./index.css"; // 
+import App from "./App";
+import "./index.css";
 
-const root = document.getElementById("root"); 
+const root = document.getElementById("root");
 
-// Check if root is not null before calling createRoot
 if (root !== null) {
   const rootElement = ReactDOM.createRoot(root);
   rootElement.render(
